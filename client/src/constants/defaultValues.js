fb19450527b4961c25745ad356dec8a240f2706d
Ved Prakash <ved@Veds-MBP.Dlink>
2020-02-05 16:06:23 +0530

    first commit

---
+++ client/src/constants/defaultValues.js
@@ -0,0 +1,33 @@
+export const subHiddenBreakpoint=1440;
+export const menuHiddenBreakpoint = 768;
+export const defaultMenuType = 'menu-sub-hidden'; //'menu-sub-hidden', 'menu-hidden', 'menu-default'
+export const defaultStartPath = '/app/departments';
+
+export const defaultLocale='en';
+export const localeOptions=[
+    {id:'en',name:'English'},
+    {id:'es',name:'Español'},
+];
+
+// export const firebaseConfig = {
+//     apiKey: "AIzaSyAVCYKTiKw3n3pnzgio8Whopk9-lxECXZE",
+//     authDomain: "vibhaag.firebaseapp.com",
+//     databaseURL: "https://vibhaag.firebaseio.com",
+//     projectId: "vibhaag",
+//     storageBucket: "vibhaag.appspot.com",
+//     messagingSenderId: "450213689259"
+// };
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyAfMX6lGFyzGbNaaj6v0_5dhJ5YlD3NnGE",
+    authDomain: "dashboard-33707.firebaseapp.com",
+    databaseURL: "https://dashboard-33707.firebaseio.com",
+    projectId: "dashboard-33707",
+    storageBucket: "dashboard-33707.appspot.com",
+    messagingSenderId: "209355282585",
+    appId: "1:209355282585:web:2506ebd2d65468f7f1c008",
+    measurementId: "G-G3HK2EZRM0"
+};
+
+export const apiUrl = "http://localhost:3000";
+export const searchPath = "/app/departments";
